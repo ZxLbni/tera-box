619955a14b552eae39089f4ed1a70be61550344a
Tobigod123 <134499497+Tobigod123@users.noreply.github.com>
2023-12-21 16:50:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 from os import environ
 from helpers.cookie import parseCookieFile
 class Config:
-    API_ID = int(environ.get("API_ID"))
-    API_HASH = environ.get("API_HASH")
-    BOT_TOKEN = environ.get("BOT_TOKEN")
+    API_ID = int(environ.get("API_ID", default=3847632))
+    API_HASH = environ.get("API_HASH", default="1a9708f807ddd06b10337f2091c67657")
+    BOT_TOKEN = environ.get("BOT_TOKEN", default="6312969509:AAE4CYabaJQceMV1fB3fRFRnbDRljk_je_c")
     COOKIES = parseCookieFile("cookies.txt")
     
